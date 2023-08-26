f5a85496ca0df64ba0ce0f4febb055a577fd6963
Andrii Zadorozhnii <zador746@gmail.com>
2023-08-26 14:40:48 -0700

    add mean calculation on change draft and density input

--- index.js
+++ index.js
@@ -4,33 +4,46 @@ let cargoName = document.querySelector('.cargo-name__input');
 let initialDate = document.querySelector('.initial-data__date-input');
 let finalDate = document.querySelector('.final-data__date-input');
 
-// Initial Drafts
-let initialDraftForward = document.querySelector(".initial-data__drafts-Df-input");
-let initialDraftMiddle = document.querySelector('.initial-data__drafts-Dm-input');
-let initialDraftAft = document.querySelector('.initial-data__drafts-Da-input');
+let initialDraftPortForward = document.querySelector(".initial-data__drafts-Port-Df-input");
+let initialDraftPortMiddle = document.querySelector('.initial-data__drafts-Port-Dm-input');
+let initialDraftPortAft = document.querySelector('.initial-data__drafts-Port-Da-input');
 
-// Final Drafts
-let finalDraftForward = document.querySelector(".final-data__drafts-Df-input");
-let finalDraftMiddle = document.querySelector('.final-data__drafts-Dm-input');
-let finalDraftAft = document.querySelector('.final-data__drafts-Da-input');
+let initialDraftStbdForward = document.querySelector(".initial-data__drafts-Stbd-Df-input");
+let initialDraftStbdMiddle = document.querySelector('.initial-data__drafts-Stbd-Dm-input');
+let initialDraftStbdAft = document.querySelector('.initial-data__drafts-Stbd-Da-input');
+
+let finalDraftPortForward = document.querySelector(".final-data__drafts-Port-Df-input");
+let finalDraftPortMiddle = document.querySelector('.final-data__drafts-Port-Dm-input');
+let finalDraftPortAft = document.querySelector('.final-data__drafts-Port-Da-input');
+
+let finalDraftStbdForward = document.querySelector(".final-data__drafts-Stbd-Df-input");
+let finalDraftStbdMiddle = document.querySelector('.final-data__drafts-Stbd-Dm-input');
+let finalDraftStbdAft = document.querySelector('.final-data__drafts-Stbd-Da-input');
+
+//Initial Mean draft calculation
+let initialDraftMeanForward = document.querySelector(".initial-data__drafts-Mean-Df");
+let initialDraftMeanMiddle = document.querySelector(".initial-data__drafts-Mean-Dm");
+let initialDraftMeanAft = document.querySelector(".initial-data__drafts-Mean-Da");
+
+// Final Mean draft calculation
+
+let finalDraftMeanForward = document.querySelector(".final-data__drafts-Mean-Df");
+let finalDraftMeanMiddle = document.querySelector(".final-data__drafts-Mean-Dm");
+let finalDraftMeanAft = document.querySelector(".final-data__drafts-Mean-Da");
+
+//density
+
+let initialWaterDensity = document.querySelector('.initial-data__density-input');
+let finalWaterDensity = document.querySelector('.final-data__density-input');
 
 
-// Load values from Local Storage if available
-vesselName.value = localStorage.getItem('vesselName') || '';
-portName.value = localStorage.getItem('portName') || '';
-cargoName.value = localStorage.getItem('cargoName') || '';
-initialDate.value = localStorage.getItem('initialDate') || '';
-finalDate.value = localStorage.getItem('finalDate') || ''
+function MeanDraftCalculation(PortDraft, StbdDraft) {
+    let meanDraft = (PortDraft + StbdDraft) / 2;
+    return meanDraft;
+}
 
-initialDraftForward.value = localStorage.getItem('initialDraftForward') || ''
-initialDraftMiddle.value = localStorage.getItem('initialDraftMiddle') || ''
-initialDraftAft.value = localStorage.getItem('initialDraftAft') || ''
 
-finalDraftForward.value = localStorage.getItem('finalDraftForward') || ''
-finalDraftMiddle.value = localStorage.getItem('finalDraftMiddle') || ''
-finalDraftAft.value = localStorage.getItem('finalDraftAft') || ''
 
-// Event listener to update Local Storage and log values
 vesselName.addEventListener('input', () => {
     localStorage.setItem('vesselName', vesselName.value);
 });
@@ -52,28 +65,125 @@ finalDate.addEventListener('input', () => {
 });
 
 // initial Drafts 
-initialDraftForward.addEventListener('input', () => {
-    localStorage.setItem('initialDraftForward', initialDraftForward.value);
+initialDraftPortForward.addEventListener('input', () => {
+    localStorage.setItem('initialDraftPortForward', initialDraftPortForward.value);
+    let meanDraft = MeanDraftCalculation(parseFloat(initialDraftPortForward.value), parseFloat(initialDraftStbdForward.value));
+    initialDraftMeanForward.textContent = meanDraft.toFixed(3);
+    localStorage.setItem('initialMeanDraftForward', meanDraft.toFixed(3));
 });
 
-initialDraftMiddle.addEventListener('input', () => {
-    localStorage.setItem('initialDraftMiddle', initialDraftMiddle.value);
+initialDraftPortMiddle.addEventListener('input', () => {
+    localStorage.setItem('initialDraftPortMiddle', initialDraftPortMiddle.value);
+    let meanDraft = MeanDraftCalculation(parseFloat(initialDraftPortMiddle.value), parseFloat(initialDraftStbdMiddle.value));
+    initialDraftMeanMiddle.textContent = meanDraft.toFixed(3);
+    localStorage.setItem('initialMeanDraftMiddle', meanDraft.toFixed(3));
 });
 
-initialDraftAft.addEventListener('input', () => {
-    localStorage.setItem('initialDraftAft', initialDraftAft.value);
+initialDraftPortAft.addEventListener('input', () => {
+    localStorage.setItem('initialDraftPortAft', initialDraftPortAft.value);
+    let meanDraft = MeanDraftCalculation(parseFloat(initialDraftPortAft.value), parseFloat(initialDraftStbdAft.value));
+    initialDraftMeanAft.textContent = meanDraft.toFixed(3);
+    localStorage.setItem('initialMeanDraftAft', meanDraft.toFixed(3));
+});
+
+
+initialDraftStbdForward.addEventListener('input', () => {
+    localStorage.setItem('initialDraftStbdForward', initialDraftStbdForward.value);
+    let meanDraft = MeanDraftCalculation(parseFloat(initialDraftPortForward.value), parseFloat(initialDraftStbdForward.value));
+    initialDraftMeanForward.textContent = meanDraft.toFixed(3);
+    localStorage.setItem('initialMeanDraftForward', meanDraft.toFixed(3));
+});
+
+initialDraftStbdMiddle.addEventListener('input', () => {
+    localStorage.setItem('initialDraftStbdMiddle', initialDraftStbdMiddle.value);
+    let meanDraft = MeanDraftCalculation(parseFloat(initialDraftPortMiddle.value), parseFloat(initialDraftStbdMiddle.value));
+    initialDraftMeanMiddle.textContent = meanDraft.toFixed(3);
+    localStorage.setItem('initialMeanDraftMiddle', meanDraft.toFixed(3));
+});
+
+initialDraftStbdAft.addEventListener('input', () => {
+    localStorage.setItem('initialDraftStbdAft', initialDraftStbdAft.value);
+    let meanDraft = MeanDraftCalculation(parseFloat(initialDraftPortAft.value), parseFloat(initialDraftStbdAft.value));
+    initialDraftMeanAft.textContent = meanDraft.toFixed(3);
+    localStorage.setItem('initialMeanDraftAft', meanDraft.toFixed(3));
 });
 
 // Final Drafts
 
-finalDraftForward.addEventListener('input', () => {
-    localStorage.setItem('finalDraftForward', finalDraftForward.value);
+finalDraftPortForward.addEventListener('input', () => {
+    localStorage.setItem('finalDraftPortForward', finalDraftPortForward.value);
+    let meanDraft = MeanDraftCalculation(parseFloat(finalDraftPortForward.value), parseFloat(finalDraftStbdForward.value));
+    finalDraftMeanForward.textContent = meanDraft.toFixed(3);
+    localStorage.setItem('finalMeanDraftForward', meanDraft.toFixed(3));
 });
 
-finalDraftMiddle.addEventListener('input', () => {
-    localStorage.setItem('finalDraftMiddle', finalDraftMiddle.value);
+finalDraftPortMiddle.addEventListener('input', () => {
+    localStorage.setItem('finalDraftPortMiddle', finalDraftPortMiddle.value);
+    let meanDraft = MeanDraftCalculation(parseFloat(finalDraftPortMiddle.value), parseFloat(finalDraftStbdMiddle.value));
+    finalDraftMeanMiddle.textContent = meanDraft.toFixed(3);
+    localStorage.setItem('finalMeanDraftMiddle', meanDraft.toFixed(3));
 });
 
-finalDraftAft.addEventListener('input', () => {
-    localStorage.setItem('finalDraftAft', finalDraftAft.value);
+finalDraftPortAft.addEventListener('input', () => {
+    localStorage.setItem('finalDraftPortAft', finalDraftPortAft.value);
+    let meanDraft = MeanDraftCalculation(parseFloat(finalDraftPortAft.value), parseFloat(finalDraftStbdAft.value));
+    finalDraftMeanAft.textContent = meanDraft.toFixed(3);
+    localStorage.setItem('finalMeanDraftAft', meanDraft.toFixed(3));
 });
+
+
+finalDraftStbdForward.addEventListener('input', () => {
+    localStorage.setItem('finalDraftStbdForward', finalDraftStbdForward.value);
+    let meanDraft = MeanDraftCalculation(parseFloat(finalDraftPortForward.value), parseFloat(finalDraftStbdForward.value));
+    finalDraftMeanForward.textContent = meanDraft.toFixed(3);
+    localStorage.setItem('finalMeanDraftForward', meanDraft.toFixed(3));
+});
+
+finalDraftStbdMiddle.addEventListener('input', () => {
+    localStorage.setItem('finalDraftStbdMiddle', finalDraftStbdMiddle.value);
+    let meanDraft = MeanDraftCalculation(parseFloat(finalDraftPortMiddle.value), parseFloat(finalDraftStbdMiddle.value));
+    finalDraftMeanMiddle.textContent = meanDraft.toFixed(3);
+    localStorage.setItem('finalMeanDraftMiddle', meanDraft.toFixed(3));
+});
+
+finalDraftStbdAft.addEventListener('input', () => {
+    localStorage.setItem('finalDraftStbdAft', finalDraftStbdAft.value);
+    let meanDraft = MeanDraftCalculation(parseFloat(finalDraftPortAft.value), parseFloat(finalDraftStbdAft.value));
+    finalDraftMeanAft.textContent = meanDraft.toFixed(3);
+    localStorage.setItem('finalMeanDraftAft', meanDraft.toFixed(3));
+});
+
+
+
+
+
+// get data from local storage
+vesselName.value = localStorage.getItem('vesselName') || '';
+portName.value = localStorage.getItem('portName') || '';
+cargoName.value = localStorage.getItem('cargoName') || '';
+initialDate.value = localStorage.getItem('initialDate') || '';
+finalDate.value = localStorage.getItem('finalDate') || '';
+
+initialDraftPortForward.value = localStorage.getItem('initialDraftPortForward') || '';
+initialDraftPortMiddle.value = localStorage.getItem('initialDraftPortMiddle') || '';
+initialDraftPortAft.value = localStorage.getItem('initialDraftPortAft') || '';
+
+initialDraftStbdForward.value = localStorage.getItem('initialDraftStbdForward') || '';
+initialDraftStbdMiddle.value = localStorage.getItem('initialDraftStbdMiddle') || '';
+initialDraftStbdAft.value = localStorage.getItem('initialDraftStbdAft') || '';
+
+finalDraftPortForward.value = localStorage.getItem('finalDraftPortForward') || '';
+finalDraftPortMiddle.value = localStorage.getItem('finalDraftPortMiddle') || '';
+finalDraftPortAft.value = localStorage.getItem('finalDraftPortAft') || '';
+
+finalDraftStbdForward.value = localStorage.getItem('finalDraftStbdForward') || '';
+finalDraftStbdMiddle.value = localStorage.getItem('finalDraftStbdMiddle') || '';
+finalDraftStbdAft.value = localStorage.getItem('finalDraftStbdAft') || '';
+
+initialDraftMeanForward.value.textContent = localStorage.getItem('initialMeanDraftPortForward') || '';
+initialDraftMeanMiddle.value.textContent = localStorage.getItem('initialMeanDraftPortMiddle') || '';
+initialDraftMeanAft.value.textContent = localStorage.getItem('initialMeanDraftPortAft') || '';
+
+finalDraftMeanForward.value.textContent = localStorage.getItem('finaMeanDraftPortForward') || '';
+finalDraftMeanMiddle.value.textContent = localStorage.getItem('finaMeanDraftPortMiddle') || '';
+finalDraftMeanAft.value.textContent = localStorage.getItem('finaMeanDraftPortAft') || '';
